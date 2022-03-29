fb4e62f3df13280668c5724915bf696b1643cc25
JSubba <prolearn1000@protonmail.com>
2022-03-29 11:09:42 +0900

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tennis Website</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        fugiat sit, architecto error esse sequi.
+      </p>
+    </div>
+  );
+}
+
+export default App;
